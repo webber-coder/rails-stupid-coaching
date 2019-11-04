dd46d799b8f4038cdcd0374e69ee1ff4991bad76
ozge <ozgezeynep08@gmail.com>
2019-11-04 21:57:01 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'ask', to: 'questions#ask'
-  get 'answer', to: 'questions#answer'
-  get 'ask/:question', to: 'question#ask'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
